34e48ac265763f358c0ffd09bac940a4d78bd971
Lucie2lr <lucie2lr@gmail.com>
2013-10-26 21:54:46 +0200

    Create Point3D and constructor

--- application/include/Point3D.hpp
+++ application/include/Point3D.hpp
@@ -3,8 +3,9 @@
 struct Point3D{
 	float x;
 	float y;
+	float z;
 	
 	Point3D();
-	Point3D(float x_, float y_);
+	Point3D(float x_, float y_, float z_);
 	Point3D(Point3D& other);
 };
--- application/src/Point3D.cpp
+++ application/src/Point3D.cpp
@@ -1,14 +1,14 @@
 #include "Point3D.hpp"
 
 Point3D::Point3D():
-			x(0.f),y(0.f){
+			x(0.f),y(0.f),z(0.f){
 				
 	}
-	Point3D::Point3D(float x_,float y_):
-			x(x_),y(y_){
+	Point3D::Point3D(float x_,float y_, float z_):
+			x(x_),y(y_),z(z_){
 				
 	}
 	Point3D::Point3D(Point3D& other):
-			x(other.x),y(other.y){
+			x(other.x),y(other.y),z(other.z){
 				
 	}
