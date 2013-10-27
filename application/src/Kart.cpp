8b462ba50a24bb9bda58e413f3086385e4ec7eeb
Lucie2lr <lucie2lr@gmail.com>
2013-10-27 18:05:45 +0100

    When the Kart moves, its direction and its position change

--- application/src/Kart.cpp
+++ application/src/Kart.cpp
@@ -13,23 +13,27 @@ Kart::Kart(glm::vec3 pos_, float speed_, glimac::Vertex2D dir_):
 // The Kart goes forward : its position changes
 void Kart::moveForward(){
 	std::cout << "J'avance" << std::endl;
-	position.y ++; //J'ai changé y mais ce ne sera pas forcément ça au final 
+	direction.y = 1.0;
+	position.y ++; 
 }
 
 //The Kart goes back : its position changes
 void Kart::moveBackward(){
 	std::cout << "Je recule" << std::endl;
+	direction.y = -1.0;
 	position.y --;
 }
 
 // The Kart goes left : its direction changes
 void Kart::turnLeft(){
 	std::cout << "Je tourne à droite" << std::endl;
-	direction.x ++;
+	direction.x = -1.0;
+	position.x --;
 }
 
 // The Kart goes left : its direction changes
 void Kart::turnRight(){
 	std::cout << "Je tourne à gauche" << std::endl;
-	direction.x --;
+	direction.x = 1.0;
+	position.x ++;
 }
