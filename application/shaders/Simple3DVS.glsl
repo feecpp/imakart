a0b8c1e6e3118e47775ee8ad59b2b01939ab530d
Lucie2lr <lucie2lr@gmail.com>
2013-12-18 21:43:11 +0100

    Passage en vec4 dans la fonction blinnPhong

--- application/shaders/Simple3DVS.glsl
+++ application/shaders/Simple3DVS.glsl
@@ -18,11 +18,11 @@ uniform mat4 viewProjection = mat4(1);
 uniform Material material;
 
 out vec4 FragColor;
-out vec3 vNormal_vs;
-out vec3 vPosition;
+out vec4 vNormal_vs;
+out vec4 vPosition;
 //En rapport avec material
-out vec3 Kd;
-out vec3 Ks;
+out vec4 Kd;
+out vec4 Ks;
 out float shine;
 
 
@@ -33,10 +33,10 @@ void main() {
 
   //Calcul des valeurs de sortie
   FragColor = material.ambient + material.diffuse + material.specular;
-  vNormal_vs = vec3(uNormal * vertexNormal);
-  vPosition = vec3(uView * model * vertexPosition);
-  Kd = material.diffuse.rgb;
-  Ks = material.specular.rgb;
+  vNormal_vs = uNormal * vertexNormal;
+  vPosition = uView * model * vertexPosition;
+  Kd = material.diffuse;
+  Ks = material.specular;
   shine = material.shininess;
 
   gl_Position = viewProjection * model * vec4(iVertexPosition, 1.f);
--- application/shaders/SimpleFS.glsl
+++ application/shaders/SimpleFS.glsl
@@ -1,11 +1,11 @@
 #version 330
 in vec4 FragColor;
-in vec3 vNormal_vs; //Normale de "l'objet"
-in vec3 vPosition; //Position en fonction de la caméra
+in vec4 vNormal_vs; //Normale de "l'objet"
+in vec4 vPosition; //Position en fonction de la caméra
 
 //////// EN RAPPORT AVEC L'OBJET //////////
-in vec3 Kd; //--> coef reflection diffuse du materiau de l'objet
-in vec3 Ks; //--> coef reflection glossy du materiau de l'objet
+in vec4 Kd; //--> coef reflection diffuse du materiau de l'objet
+in vec4 Ks; //--> coef reflection glossy du materiau de l'objet
 in float shine; //--> controlle la taille de la "tache" de brillance (en fonction du materiau de l'objet)
 
 
@@ -21,24 +21,22 @@ out vec4 oFragColor;
 
 //Calcul de la luminosite
 vec4 blinnPhong() {
-  vec3 wi = normalize(uLightDir);//Vecteur qui pointe vers la lumière
-  vec3 w0 = normalize(-vPosition);//Vecteur qui pointe vers la camera
-  vec3 halfVector = (w0+wi)*0.5f;
+  vec4 wi = normalize(vec4(uLightDir,1.0));//Vecteur qui pointe vers la lumière
+  vec4 w0 = normalize(-vPosition);//Vecteur qui pointe vers la camera
+  vec4 halfVector = (w0+wi)*0.5f;
 
-  vec3 light = uLi*( Kd*dot(wi,vNormal_vs) +  Ks*pow(dot(halfVector,vNormal_vs),shine));
-  vec4 res = vec4(light,1.0);
+  vec4 res = vec4(uLi,1.0)*( Kd*dot(wi,vNormal_vs) +  Ks*pow(dot(halfVector,vNormal_vs),shine));
 
   return res;
 }
 
 vec4 blinnPhongPonctuelle(){
-    vec3 wi = normalize(uLightPos-vPosition);//Vecteur qui pointe vers la lumière
-    vec3 w0 = normalize(-vPosition);//Vecteur qui pointe vers la camera
-    vec3 halfVector = (w0+wi)*0.5f;
+    vec4 wi = normalize(vec4(uLightDir,1.0)-vPosition);//Vecteur qui pointe vers la lumière
+    vec4 w0 = normalize(-vPosition);//Vecteur qui pointe vers la camera
+    vec4 halfVector = (w0+wi)*0.5f;
     float d = length(wi);
 
-    vec3 light = uLi*( Kd*dot(wi,vNormal_vs) +  Ks*pow(dot(halfVector,vNormal_vs),shine))/(d*d);
-    vec4 res = vec4(light,1.0);
+    vec4 res = vec4(uLi,1.0)*( Kd*dot(wi,vNormal_vs) +  Ks*pow(dot(halfVector,vNormal_vs),shine))/(d*d);
 
     return res;
 }
