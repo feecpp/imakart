b86082efeb8c01118ab6f135b605aff8e7ce959f
Lucie2lr <lucie2lr@gmail.com>
2013-12-28 12:41:02 +0100

    On récupère les éléments du material dans le FS grâce à l'uniform

--- application/shaders/SimpleFS.glsl
+++ application/shaders/SimpleFS.glsl
@@ -1,14 +1,17 @@
 #version 330
 in vec4 FragColor;
 in vec4 vNormal_vs; //Normale de "l'objet"
-in vec4 vPosition; //Position en fonction de la caméra
+in vec4 vPosition_vs; //Position en fonction de la caméra
 
 //////// EN RAPPORT AVEC L'OBJET //////////
-in vec4 Kd; //--> coef reflection diffuse du materiau de l'objet
-in vec4 Ks; //--> coef reflection glossy du materiau de l'objet
-in vec4 Ka;
-in float shine; //--> controlle la taille de la "tache" de brillance (en fonction du materiau de l'objet)
-
+struct Material
+{
+  vec4 ambient;
+  vec4 diffuse;
+  vec4 specular;
+  float shininess;
+};
+uniform Material material;
 
 uniform mat4 model = mat4(1);
 /////// EN RAPPORT AVEC LA SOURCE DE LUMIERE ////////
@@ -23,21 +26,21 @@ out vec4 oFragColor;
 //Calcul de la luminosite
 vec4 blinnPhong() {
   vec4 wi = normalize(vec4(uLightDir,1.0));//Vecteur qui pointe vers la lumière
-  vec4 w0 = normalize(-vPosition);//Vecteur qui pointe vers la camera
+  vec4 w0 = normalize(-vPosition_vs);//Vecteur qui pointe vers la camera
   vec4 halfVector = (w0+wi)*0.5f;
 
-  vec4 res = vec4(uLi,1.0)*( Kd*dot(wi,vNormal_vs) +  Ks*pow(dot(halfVector,vNormal_vs),shine) + Ka);
+  vec4 res = vec4(uLi,1.0)*( material.diffuse*dot(wi,vNormal_vs) +  material.specular*pow(dot(halfVector,vNormal_vs),material.shininess) + material.ambient);
 
   return res;
 }
 
 vec4 blinnPhongPonctuelle(){
-    vec4 wi = normalize(vec4(uLightPos,1.0)-vPosition);//Vecteur qui pointe vers la lumière
-    vec4 w0 = normalize(-vPosition);//Vecteur qui pointe vers la camera
+    vec4 wi = normalize(vec4(uLightPos,1.0)-vPosition_vs);//Vecteur qui pointe vers la lumière
+    vec4 w0 = normalize(-vPosition_vs);//Vecteur qui pointe vers la camera
     vec4 halfVector = (w0+wi)*0.5f;
     float d = length(wi);
 
-    vec4 res = vec4(uLi,1.0)*( Kd*dot(wi,vNormal_vs) +  Ks*pow(dot(halfVector,vNormal_vs),shine) + Ka)/(d*d);
+    vec4 res = vec4(uLi,1.0)*( material.diffuse*dot(wi,vNormal_vs) +  material.specular*pow(dot(halfVector,vNormal_vs),material.shininess) + material.ambient)/(d*d);
 
     return res;
 }
