be72e5a3163c32a04ec9ee7ce950f42f4d127870
Lucie2lr <lucie2lr@gmail.com>
2013-12-30 10:56:40 +0100

    Tests pour trouver la formule de la lumière directionnelle

--- application/shaders/SimpleFS.glsl
+++ application/shaders/SimpleFS.glsl
@@ -29,9 +29,37 @@ vec4 blinnPhong() {
   vec4 w0 = normalize(-vPosition_vs);//Vecteur qui pointe vers la camera
   vec4 halfVector = (w0+wi)*0.5f;
 
-  vec4 res = vec4(uLi,1.0)*( material.diffuse*dot(wi,vNormal_vs) +  material.specular*pow(dot(halfVector,vNormal_vs),material.shininess) + material.ambient);
+  vec4 res = vec4(uLi,1.0)*( material.diffuse*max(dot(wi,vNormal_vs),0.0) +  material.specular*pow(max(dot(halfVector,vNormal_vs),0.0),material.shininess) + material.ambient);
 
   return res;
+}
+
+vec4 BP(){
+    // set the specular term to black
+    vec4 spec = vec4(0.0);
+    vec4 diff = vec4(0.0);
+    vec4 amb = vec4(0.0);
+
+    // normalize both input vectors
+    vec3 n = normalize(vNormal_vs.rgb);
+    vec3 e = normalize(-vPosition_vs.rgb);
+
+    float intensity = max(dot(n,uLightDir), 0.0);
+    //float intensity = 0.1f;
+
+    // if the vertex is lit compute the specular color
+    if (intensity > 0.0) {
+        // compute the half vector
+        vec3 h = normalize((uLightDir + e)*0.5f);
+        // compute the specular term into spec
+        float intSpec = max(dot(h,n), 0.0);
+        spec = material.specular * pow(intSpec,material.shininess);
+        diff = material.diffuse * max(dot(n,uLightDir), 0.0);
+        amb = material.ambient * intensity;
+    }
+
+    vec4 res = max(intensity * material.diffuse + spec, material.ambient);
+    return res;
 }
 
 vec4 ambientDiffuse()
@@ -43,25 +71,6 @@ vec4 ambientDiffuse()
   vec4 vAmbientColor = ambientLight * material.ambient;
 
   return vAmbientColor + vDiffuseColor;
-}
-
-vec4 ADS()
-{
-    // Dot product gives us diffuse intensity
-    float diff = max(0.0, dot(normalize(vNormal_vs),normalize(vec4(uLightDir, 0.f))));
-    // Multiply intensity by diffuse color, force alpha to 1.0
-    vec4 res = diff * material.diffuse;
-    // Add in ambient light
-    res += material.ambient;
-    // Specular Light
-    vec4 vReflection = normalize(reflect(-normalize(vec4(uLightDir, 0.f)),normalize(vNormal_vs)));
-    float spec = max(0.0, dot(normalize(vNormal_vs), vReflection));
-    // If the diffuse light is zero, don’t even bother with the pow function
-    if(diff != 0) {
-        float fSpec = pow(spec, 128.0);
-        res.rgb += vec3(fSpec, fSpec, fSpec);
-    }
-    return res;
 }
 
 vec4 blinnPhongPonctuelle(){
@@ -78,5 +87,5 @@ vec4 blinnPhongPonctuelle(){
 void main(void)
 {
   //oFragColor = FragColor;
-  oFragColor = ADS();
+  oFragColor = ambientDiffuse();
 }
