c5da02ba227e642c1cab7b44cdbc53456def6b18
Jonathan Douet <jonathandouet@hotmail.fr>
2014-01-07 22:01:36 +0100

    Fix pour le déplacement de l'adversaire

--- application/src/Opponent.cpp
+++ application/src/Opponent.cpp
@@ -35,21 +35,11 @@ void Opponent::validateCheckpoints()
   {
     if (it->contains(opponentKart.getPosition()))
     {
-      //je vais chercher le checkpoint valide
-      //std::cout <<"position du checkpoint valide x : "<< it->position.x << std::endl;
-     // std::cout <<"position du checkpoint valide z : "<< it->position.z << std::endl;
-      std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
-      std::cout <<"checkpoint actuel : "<< cpt << std::endl;
-
-      //je vais chercher le checkpoint suivant
       if(cpt == checkpoints.size()-1){
         cpt = 0;
       }else{
         cpt++;
       }
-
-      std::cout <<"checkpoint suivant : "<< cpt<< std::endl;
-      std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
 
       x = checkpoints[cpt].position.x - opponentKart.getPosition().x;
       z = checkpoints[cpt].position.z - opponentKart.getPosition().z;
@@ -66,26 +56,17 @@ void Opponent::validateCheckpoints()
     angle += 360.f;
   }
 
-  std::cout << "----------------------------------------------------------" << std::endl;
-  std::cout <<"x  "<< x << std::endl;
-  std::cout <<"z  "<< z << std::endl;
-  std::cout <<"angle  "<< angle << std::endl;
-
   float qx = opponentKart.getOrientation().x;
   float qy = opponentKart.getOrientation().y;
   float qz = opponentKart.getOrientation().z;
   float qw = opponentKart.getOrientation().w;
 
-
   heading = (atan2(2*qy*qw-2*qx*qz , 1 - 2*qy*qy - 2*qz*qz)) * 360.f / (2*M_PI);
-  std::cout <<"heading  "<< heading << std::endl;
 
-
+  opponentKart.moveForward();
 
   if(angle != 0.f){
       if(heading <= (angle -1.5f) || heading >= (angle +1.5f)){
-        std::cout <<"right " << std::endl;
-        opponentKart.moveForward();
         if(heading <= 0 && angle <= 0){
           if(fabs(heading) - fabs(angle) <0){
             opponentKart.turnRight();
@@ -112,11 +93,9 @@ void Opponent::validateCheckpoints()
           }
         }
       }else{
-        std::cout <<"dontmove " << std::endl;
         opponentKart.stopTurning();
       }
   }
-  std::cout << "----------------------------------------------------------" << std::endl;
 }
 
 void Opponent::startMovement(){
